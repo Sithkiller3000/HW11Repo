323a732eb28643fba5d2ad17ffc7090ec736e93b
Sithkiller3000 <tim7lucas@gmail.com>
2023-01-08 18:13:41 +0100

    Main.java bearbeitet

--- src/hw11/Main.java
+++ src/hw11/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Code at line 6");
 		System.out.println("Code at line 7, changed in master");
+		int x = 10;
+		int y = 2;
+		if(x > y) {
+			System.out.println("Hello World");
+		}
 	}
 
 }
